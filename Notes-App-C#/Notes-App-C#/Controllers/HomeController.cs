a8d92b76954fd09315c079e6cf811dc785692cbd
František Brikner <briknerf22@spstrutnov.cz>
2026-03-19 10:19:05 +0100

    založení ASP.NET Core MVC projektu

---
+++ Notes-App-C#/Notes-App-C#/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Notes_App_C_.Models;
+using System.Diagnostics;
+
+namespace Notes_App_C_.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
